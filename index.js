02d19634d48b39db73b3d09819f4ced991bd7894
Junki <wnsrl1228@naver.com>
2022-03-15 18:58:19 +0900

    로그인 정보 추가

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const app = express()
 const port = 5000
 
 const config = require('./config/key');
-
+const cookieParser = require('cookie-parser');
+const {auth} =require('./middleware/auth')
 const { User } = require("./models/Users")
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) 
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -23,18 +24,78 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //회원 가입
 
     const user = new User(req.body)
 
+
     user.save((err,userInfo)=>{
         if(err) return res.json({success:false,err})
         return res.status(200).json({
-            success:true
+            success:true,
         })
     })
   })
+
+app.post('/api/users/login',(req,res) => {
+  
+  //요청된 이메일이 db에 있는지 확인
+  User.findOne({email: req.body.email }, (err,user) => {
+    if(!user){
+      return res.json({
+        loginSuccess:false,
+        message:"제공된 이메일에 해당하는 유저가 없다."
+      })
+    }
+
+    //요청된 이메일이 있다면 비번이 맞는지 확인
+    user.comparePassword(req.body.password , (err,isMatch)=>{
+      if(!isMatch)
+        return res.json({loginSuccess:false,message:"비번틀림"})
+
+
+      // 비밀번호까지 맞으면 토큰생성
+      user.generateToken((err,user)=>{
+        if(err) return res.status(400).send(err)
+
+        // 토큰을 저장한다 어디에? 쿠키, 로컬저장소
+        res.cookie("x_auth",user.token)
+        .status(200)
+        .json({loginSuccess:true,userId:user._id})  
+      })
+
+    })
+  })
+})
+
+app.get("/api/users/auth", auth,(req,res) => {
+
+  //여기까지 미드웨어를 통과해 왔다는 얘기는 권환 통과
+  res.json(200).json({
+    _id:req.user._id,
+    isAdmin:req.user.role===0?false:true,
+    isAuth:true,
+    email:req.user.email,
+    name:req.user.name,
+    lastname:req.user.lastname,
+    role:req.user.role,
+    image:req.user.image
+  })
+})
+
+
+app.get('/api/users/logout',auth,(req,res)=>{
+
+  User.findOneAndUpdate({_id: req.user._id},
+    {token:""}
+    , (err,user) => {
+      if (err) return res.json({success:false,err});
+      return res.status(200).send({
+        success:true
+      })
+    })
+})
 
 // 앱이 실행되면 실행
 app.listen(port, () => {
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// const res = require('express/lib/response');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name : {
@@ -32,9 +37,62 @@ const userSchema = mongoose.Schema({
 
 })
 
+userSchema.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password')){
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else{
+        next()
+    }
+    
+    
+})
+userSchema.methods.comparePassword = function(plainPassword,cb){
+
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err)
+        cb(null,isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    var user = this
+    //jsonwebtoken 이용해서 토큰생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken = function(token,cb){
+    var user = this
+
+    //토큰을 decode 한다.
+    jwt.verify(token,'secretToken',function(err,decoded){
+        //유저아이디를 이용해서 유저 찾고 비교
+        user.findOne({"_id":decoded,"token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
+}
+
+
 const User = mongoose.model('User',userSchema)
 
 module.exports = {User}
 
 // 스키마 작성 , 모델에 스키마 넣어줌, 모듈 다른 곳에서 쓸 수 있도록 해줌
-
